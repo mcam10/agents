0ad615ff0b380adc6c9a86b2b1b038f9eb2c7cab
Malik Cameron <malikcameron95@gmail.com>
2024-11-08 14:12:14 -0800

    More code usage

--- aws_tool.py
+++ aws_tool.py
@@ -1,10 +1,16 @@
 import os
 import subprocess
-from autogen import AssistantAgent, UserProxyAgent,config_list_from_json,register_function
+from autogen import AssistantAgent, UserProxyAgent, ConversableAgent, config_list_from_json,register_function
+from autogen.coding import LocalCommandLineCodeExecutor
 
 from typing_extensions import Annotated
 
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+
+executor = LocalCommandLineCodeExecutor(
+    timeout=10,  # Timeout for each code execution in seconds.
+    functions = list_objects_in_account,
+)
 
 
 llm_config = {
@@ -15,12 +21,13 @@ llm_config = {
 engineer = AssistantAgent(
     name="Engineer",
     llm_config=llm_config,
+    code_execution_config ={"executor":executor},
     system_message="""
     I'm Engineer. I'm expert in bash programming. I'm executing code tasks required by Admin.
     """,
 )
 
-user_proxy = UserProxyAgent(
+user_proxy = ConversableAgent(
     name="Admin",
     human_input_mode="ALWAYS",
     code_execution_config=False,
@@ -31,11 +38,12 @@ user_proxy = UserProxyAgent(
 #### prepare appropiate functions
 
 def list_objects_in_account(directory: Annotated[str, "AWS Account to check"]):
-    subprocess.run(["aws","s3","ls"], shell=True)
+    subprocess.run(["aws s3 ls"], shell=True)
 
 #Register the function with the agents
 
-engineer.register_for_llm(name="list_objects_in_account",description="List buckets",(list_objects_in_account)
-user_proxy_agent.register_for_execution(name="list_objects_in_account",description="List buckets",(list_objects_in_account)
+engineer.register_for_llm(name="list_objects_in_account",description="List buckets")(list_objects_in_account)
+
+user_proxy.register_for_execution(name="list_objects_in_account")(list_objects_in_account)
 
 chat_result = user_proxy.initiate_chat(engineer, message="List AWS S3 buckets")
