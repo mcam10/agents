346c0c710bbbab819d2b55c0b0baec09974842eb
Malik Cameron <malikcameron95@gmail.com>
2024-11-08 21:02:24 -0800

    Getting a return object type

--- aws_tool.py
+++ aws_tool.py
@@ -8,8 +8,13 @@ from typing_extensions import Annotated
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
 
 def list_objects_in_account():
-    command = subprocess.run(["ls"], shell=True,capture_output=True)
-    return command.stdout
+    command = subprocess.run(["ls"])
+    return command
+
+executor = LocalCommandLineCodeExecutor(
+    timeout=10,  # Timeout for each code execution in seconds.
+    functions=[list_objects_in_account],  # Use the temporary directory to store the code files.
+)
 
 
 llm_config = {
@@ -29,8 +34,7 @@ assistant = ConversableAgent(
     name="Assistant",
     code_execution_config=False,
     llm_config=llm_config ,
-    system_message="You are a helpful AI assistant. "
-    "You can help with simple calculations. "
+    system_message="You are a helpful AI assistant"
     "Return 'TERMINATE' when the task is done.",
 )
 
@@ -38,17 +42,17 @@ assistant = ConversableAgent(
 
 #Register the function with the agents
 
-assistant.register_for_llm(name="list_objects_in_account",description="List buckets")(list_objects_in_account)
+#assistant.register_for_llm(name="list_objects_in_account",description="List buckets")(list_objects_in_account)
 
 #user_proxy.register_for_execution(name="list_objects_in_account")(list_objects_in_account)
-engineer.register_for_execution(name="list_objects_in_account")(list_objects_in_account)
+#engineer.register_for_execution(name="list_objects_in_account")(list_objects_in_account)
 
 register_function(
-    list_s3_buckets,
+    list_objects_in_account,
     caller=assistant,  # The assistant agent can suggest calls to the calculator.
-    executor=engineer,  # The user proxy agent can execute the calculator calls.
-    name="calculator",  # By default, the function name is used as the tool name.
-    description="A s3 bucket lister",  # A description of the tool.
+    executor=engineer,  # The engineer agent can execute the calculator calls.
+    name="list_objects_in_account",  # By default, the function name is used as the tool name.
+    description="Listerr",  # A description of the tool.
 )
 
 chat_result = engineer.initiate_chat(assistant, message="List AWS S3 buckets")
