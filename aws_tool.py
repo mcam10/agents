2fbce670d515f05407baba9c052f0570807747e9
Malik Cameron <malikcameron95@gmail.com>
2024-11-10 14:40:36 -0800

    Getting output

--- aws_tool.py
+++ aws_tool.py
@@ -8,14 +8,8 @@ from typing_extensions import Annotated
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
 
 def list_objects_in_account():
-    command = subprocess.run(["ls"])
-    return command
-
-executor = LocalCommandLineCodeExecutor(
-    timeout=10,  # Timeout for each code execution in seconds.
-    functions=[list_objects_in_account],  # Use the temporary directory to store the code files.
-)
-
+    command = subprocess.run(["ls", "-lah"], shell=True, capture_output=True, text=True)
+    return command.stdout
 
 llm_config = {
     "temperature": 0,
@@ -25,7 +19,6 @@ llm_config = {
 engineer = ConversableAgent(
     name="Engineer",
     llm_config=False,
-    code_execution_config ={"executor":executor},
     is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
     human_input_mode="NEVER",
 )
@@ -38,21 +31,12 @@ assistant = ConversableAgent(
     "Return 'TERMINATE' when the task is done.",
 )
 
-#### prepare appropiate functions
-
-#Register the function with the agents
-
-#assistant.register_for_llm(name="list_objects_in_account",description="List buckets")(list_objects_in_account)
-
-#user_proxy.register_for_execution(name="list_objects_in_account")(list_objects_in_account)
-#engineer.register_for_execution(name="list_objects_in_account")(list_objects_in_account)
-
 register_function(
     list_objects_in_account,
     caller=assistant,  # The assistant agent can suggest calls to the calculator.
     executor=engineer,  # The engineer agent can execute the calculator calls.
     name="list_objects_in_account",  # By default, the function name is used as the tool name.
-    description="Listerr",  # A description of the tool.
+    description="Lister",  # A description of the tool.
 )
 
-chat_result = engineer.initiate_chat(assistant, message="List AWS S3 buckets")
+chat_result = engineer.initiate_chat(assistant, message="List Directories")
